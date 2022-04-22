13fab2ea163dfb80d288db2528182d5a20196055
Jpremkumar <Johny.Premkumar@xpo.com>
2022-04-22 14:09:40 +0530

    first commit with maven file

---
+++ src/main/java/com/cat/app/SpringbootCatrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.cat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCatrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCatrestapiApplication.class, args);
+	}
+
+}
